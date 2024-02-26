32ca7f90d4f2c4e578b789c9cd50c7a1cfabd73f
lsumin223 <sumin99223@gmail.com>
2024-02-26 12:17:48 +0900

    타이틀 화면 제작 중

---
+++ Assets/Scripts/System/GameManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
